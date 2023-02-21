2156ba3f8025fdec2a43235928fd29f0f154c736
Vukasin Mladzic <vukasin.ml2@gmail.com>
2023-02-21 11:16:38 +0100

    final

--- Books/script.js
+++ Books/script.js
@@ -16,22 +16,41 @@ const searchInput = document.querySelector(".header-search input");
 const searchButton = document.querySelector(".header-search button");
 const mostReviewsHeading = document.querySelector(".most-reviews-heading");
 const checkbox = document.querySelector(".checkbox");
+const checkboxLabel = document.querySelector("label");
 
 let allGenres = [];
+const adultGenres = [
+  "adult",
+  "lgbt",
+  "erotic",
+  "lgbt",
+  "glbt",
+  "abuse",
+  "alcohol",
+  "lesbian",
+  "gay",
+];
 let books;
+let filteredBooks;
 
 let averageRating = 0;
 
+let activePage = "Home";
+
+// const getBooksData = async () => {
+//   return (
+//     await fetch("https://api.jsonbin.io/v3/b/63a0e753dfc68e59d56c71ec/latest", {
+//       method: "GET",
+//       headers: {
+//         "X-Master-Key":
+//           "$2b$10$viPOiL/.5Te1ctsEnmquLuBHKGeK09Vp0SxT2m7wkH68/e1537nUK",
+//       },
+//     })
+//   ).json();
+// };
+
 const getBooksData = async () => {
-  return (
-    await fetch("https://api.jsonbin.io/v3/b/63a0e753dfc68e59d56c71ec/latest", {
-      method: "GET",
-      headers: {
-        "X-Master-Key":
-          "$2b$10$viPOiL/.5Te1ctsEnmquLuBHKGeK09Vp0SxT2m7wkH68/e1537nUK",
-      },
-    })
-  ).json();
+  return (await fetch("./books.json")).json();
 };
 
 const createCard = (book, category, container) => {
@@ -40,7 +59,7 @@ const createCard = (book, category, container) => {
   const cardName = document.createElement("div");
   const cardRating = document.createElement("div");
 
-  cardImage.setAttribute("src", book.img);
+  cardImage.setAttribute("src", book.img || "./images/book.jpg");
   cardName.innerHTML = book.title;
   cardRating.innerHTML = `Rating : ${book.rating}`;
   card.classList.add(`${category}-item`);
@@ -69,18 +88,28 @@ const createBookInfo = (book) => {
   singleBookInfo.append(title, genre, pages, rating);
 };
 
-const showBooks = (books) => {
+const showBookInfo = (book, index, container) => {
+  document
+    .querySelectorAll(`.${container}`)
+    [index].addEventListener("click", () => {
+      singleBook.style.display = "unset";
+      if (activePage === "Books") {
+        allBooks.style.display = "none";
+      } else {
+        homePage.style.display = "none";
+      }
+
+      singleBookImage.setAttribute("src", book.img || "./images/book.jpg");
+      createBookInfo(book);
+      singleBookDescription.innerHTML = `Description: </br> </br>${book.desc}`;
+    });
+};
+
+const showBooks = (books, container) => {
+  allBooksItems.innerHTML = "";
   books.forEach((book, index) => {
     createCard(book, "books-all", allBooksItems);
-    document
-      .querySelectorAll(".books-all-item")
-      [index].addEventListener("click", () => {
-        singleBook.style.display = "unset";
-        allBooks.style.display = "none";
-        singleBookImage.setAttribute("src", book.img);
-        createBookInfo(book);
-        singleBookDescription.innerHTML = `Description: </br> </br>${book.desc}`;
-      });
+    showBookInfo(book, index, container);
   });
 };
 
@@ -91,20 +120,30 @@ const createGenresList = (genre) => {
 };
 
 const search = () => {
-  if (getComputedStyle(allBooks).display === "none") {
-    allBooks.style.display = "flex";
-    homePage.style.display = "none";
+  if (searchInput.value) {
+    if (getComputedStyle(allBooks).display === "none") {
+      allBooks.style.display = "flex";
+      homePage.style.display = "none";
+    }
+    // let searchedBook = books.record.results.filter((book) => {
+    // return book.title.toLowerCase().includes(searchInput.value.toLowerCase());
+    // });
+    let searchedBook = books.results.filter((book) => {
+      if (book.title) {
+        return book.title
+          .toString()
+          .toLowerCase()
+          .includes(searchInput.value.toLowerCase());
+      }
+    });
+    if (searchedBook.length > 0) {
+      allBooksItems.innerHTML = "";
+      showBooks(searchedBook, "books-all-item");
+    } else {
+      alert("There is no such book!");
+    }
+    searchInput.value = "";
   }
-  let searchedBook = books.record.results.filter((book) => {
-    return book.title.toLowerCase().includes(searchInput.value.toLowerCase());
-  });
-  if (searchedBook.length > 0) {
-    allBooksItems.innerHTML = "";
-    showBooks(searchedBook);
-  } else {
-    alert("There is no such book!");
-  }
-  searchInput.value = "";
 };
 
 const filterByGenre = () => {
@@ -112,18 +151,33 @@ const filterByGenre = () => {
     genre.addEventListener("click", () => {
       currentGenre = genre.innerHTML;
 
-      let filteredBooks = books.record.results.filter((book) =>
+      // let filteredBooksByGenre = books.record.results.filter((book) =>
+      //   book.genre.includes(genre.innerHTML)
+      // );
+      let filteredBooksByGenre = books.results.filter((book) =>
         book.genre.includes(genre.innerHTML)
       );
 
       allBooksItems.innerHTML = "";
       if (genre.innerHTML === "Default") {
-        showBooks(books.record.results);
+        // showBooks(books.record.results);
+        if (checkbox.checked) {
+          showBooks(books.results, "books-all-item");
+        } else {
+          showBooks(filteredBooks, "books-all-item");
+        }
       } else {
-        showBooks(filteredBooks);
+        showBooks(filteredBooksByGenre, "books-all-item");
       }
     });
   });
+};
+
+const defaultGenre = () => {
+  genres.innerHTML = "Genres :";
+  const defaultGenreP = document.createElement("p");
+  defaultGenreP.innerHTML = "Default";
+  genres.append(defaultGenreP);
 };
 
 window.addEventListener("load", async () => {
@@ -133,13 +187,31 @@ window.addEventListener("load", async () => {
     throw new Error(err);
   }
 
-  books.record.results.forEach((book) => {
+  filteredBooks = books.results.filter((book) => {
+    return !adultGenres.some((genre) => {
+      return book.genre.toLowerCase().includes(genre);
+    });
+  });
+
+  // books.record.results.forEach((book) => {
+  //   averageRating += book.rating;
+  // });
+  books.results.forEach((book) => {
     averageRating += book.rating;
   });
 
-  averageRating = averageRating / books.record.results.length;
+  // averageRating = averageRating / books.record.results.length;
+  averageRating = averageRating / books.results.length;
 
-  books.record.results.forEach((book) => {
+  // books.record.results.forEach((book) => {
+  // const bookGenres = book.genre.split(",");
+  // bookGenres.forEach((genre) => {
+  // if (!allGenres.includes(genre)) {
+  // allGenres = [...allGenres, genre];
+  // }
+  // });
+  // });
+  books.results.forEach((book) => {
     const bookGenres = book.genre.split(",");
     bookGenres.forEach((genre) => {
       if (!allGenres.includes(genre)) {
@@ -150,13 +222,19 @@ window.addEventListener("load", async () => {
 
   const randomGenreIndex = Math.floor(Math.random() * allGenres.length - 1);
   const randomGenre = allGenres[randomGenreIndex];
-  let booksWithGenre = books.record.results.filter((book) =>
+  // let booksWithGenre = books.record.results.filter((book) =>
+  // book.genre.includes(randomGenre)
+  // );
+  let booksWithGenre = books.results.filter((book) =>
     book.genre.includes(randomGenre)
   );
 
   mostReviewsHeading.innerHTML += ` by genre "${randomGenre}"`;
 
-  const highestRatedBooks = [...books.record.results].sort((a, b) => {
+  // const highestRatedBooks = [...books.record.results].sort((a, b) => {
+  //   return a.rating < b.rating ? 1 : a.rating > b.rating ? -1 : 0;
+  // });
+  const highestRatedBooks = [...books.results].sort((a, b) => {
     return a.rating < b.rating ? 1 : a.rating > b.rating ? -1 : 0;
   });
 
@@ -171,6 +249,7 @@ window.addEventListener("load", async () => {
         "best-rating-container",
         bestRatingContainer
       );
+      showBookInfo(highestRatedBooks[i], i, "best-rating-container-item");
     }
     if (mostReviewsBooks[i]) {
       createCard(
@@ -178,12 +257,15 @@ window.addEventListener("load", async () => {
         "most-reviews-container",
         mostReviewsContainer
       );
+      showBookInfo(mostReviewsBooks[i], i, "most-reviews-container-item");
     }
   }
 
   allGenres
-    .filter((genre) => {
-      return !genre.toLowerCase().includes("adult", "lgbt", "erotic");
+    .filter((allGenre) => {
+      return !adultGenres.some((genre) => {
+        return allGenre.toLowerCase().includes(genre);
+      });
     })
     .sort()
     .forEach((genre) => {
@@ -192,24 +274,40 @@ window.addEventListener("load", async () => {
 });
 
 navigationBooks.addEventListener("click", () => {
+  checkbox.style.display = "unset";
+  checkboxLabel.style.display = "unset";
+  activePage = "Books";
   allBooks.style.display = "flex";
   homePage.style.display = "none";
   singleBook.style.display = "none";
   currentGenre = "";
-  showBooks(books.record.results);
+  // showBooks(books.record.results);
+  if (checkbox.checked) {
+    showBooks(books.results, "books-all-item");
+  } else {
+    showBooks(filteredBooks, "books-all-item");
+  }
+
   search();
 
   filterByGenre();
 });
 
 navigationHome.addEventListener("click", () => {
+  checkbox.style.display = "none";
+  checkboxLabel.style.display = "none";
+  activePage = "Home";
   allBooks.style.display = "none";
   homePage.style.display = "flex";
 });
 
 back.addEventListener("click", () => {
   singleBook.style.display = "none";
-  allBooks.style.display = "flex";
+  if (activePage === "Books") {
+    allBooks.style.display = "flex";
+  } else {
+    homePage.style.display = "flex";
+  }
 });
 
 searchButton.addEventListener("click", search);
@@ -223,13 +321,19 @@ checkbox.addEventListener("change", () => {
   genres.innerHTML = "";
 
   if (checkbox.checked) {
+    showBooks(books.results, "books-all-item");
+    defaultGenre();
     allGenres.sort().forEach((genre) => {
       createGenresList(genre);
     });
   } else {
-    let filteredGenres = allGenres.filter((genre) => {
-      return !genre.toLowerCase().includes("adult", "lgbt", "erotic");
+    showBooks(filteredBooks, "books-all-item");
+    let filteredGenres = allGenres.filter((allGenre) => {
+      return !adultGenres.some((genre) => {
+        return allGenre.toLowerCase().includes(genre);
+      });
     });
+    defaultGenre();
     filteredGenres.sort().forEach((genre) => {
       createGenresList(genre);
     });
